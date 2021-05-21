3a6913c3f8e9195057ede49dc8b9e8d4066b6812
Watcharavit Viwatsukpaisral <watcha.ea@hotmail.com>
2021-05-21 14:35:40 +0700

    create endScreen and make ui for home screen/ help screen

--- src/application/HelpScreen.java
+++ src/application/HelpScreen.java
@@ -1,21 +1,58 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-class HelpScreen extends VBox {
+class HelpScreen extends StackPane {
+
 	public HelpScreen(Main mainInstance) {
-		this.setAlignment(Pos.TOP_CENTER);
-		Label title = new Label("Help");
-
+		
+		VBox howToPlay = new VBox();
+		howToPlay.setAlignment(Pos.TOP_CENTER);
+		howToPlay.setPrefHeight(600);
+		howToPlay.setPrefWidth(780);
+		
+		Label title = new Label("How to play");
+		title.setFont(Font.font("Courier New", FontWeight.BOLD, 36));
+		title.setPadding(new Insets(100, 20, 50, 20));
+		// add how to play
+		String manualInformation = "manual"; 
+		Label manual = new Label(manualInformation);
+		manual.setPadding(new Insets(10, 20, 100, 20));
+		
+		HBox buttonPane = new HBox();
+		buttonPane.setPrefHeight(150);
+		buttonPane.setPrefWidth(780);
+		
 		Button backButton = new Button("Back");
+		backButton.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+		backButton.setPrefHeight(40);
+		backButton.setPrefWidth(70);
 		backButton.setOnAction((ActionEvent e) -> {
 			mainInstance.switchToHome();
 		});
-
-		this.getChildren().addAll(title, backButton);
+		
+		Button playButton = new Button("Play");
+		playButton.setOnAction((ActionEvent e) -> {
+			mainInstance.switchToGame();
+		});
+		playButton.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+		playButton.setPrefHeight(40);
+		playButton.setPrefWidth(70);
+		
+		buttonPane.getChildren().addAll(backButton, playButton);
+		buttonPane.setAlignment(Pos.BOTTOM_CENTER);
+		
+		howToPlay.getChildren().addAll(title, manual, buttonPane);
+		
+		this.getChildren().addAll(howToPlay);
 	}
 }
--- src/application/HomeScreen.java
+++ src/application/HomeScreen.java
@@ -1,30 +1,53 @@
 package application;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
-class HomeScreen extends VBox {
+class HomeScreen extends StackPane {
 	private final Main mainInstance;
 
 	private final Button gameButton, helpButton;
-
+	
 	protected HomeScreen(Main mainInstance) {
 		this.mainInstance = mainInstance;
-
-		this.setAlignment(Pos.CENTER);
-
+		//StackPane container = new StackPane();
+		
+		VBox home = new VBox();
+		home.setPrefHeight(600);
+		home.setPrefWidth(780);
+		home.setAlignment(Pos.TOP_CENTER);
+		
+		Label title = new Label("King of Bomb");
+		title.setFont(Font.font("Courier New", FontWeight.BOLD, 36));
+		title.setPadding(new Insets(200, 20, 120, 20));
+		
 		gameButton = new Button("Start Game");
 		gameButton.setOnAction((ActionEvent e) -> {
 			this.switchToGame();
 		});
+		gameButton.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+		gameButton.setPrefHeight(40);
+		gameButton.setPrefWidth(150);
+		
 		helpButton = new Button("How to Play");
 		helpButton.setOnAction((ActionEvent e) -> {
 			this.switchToHelp();
 		});
+		helpButton.setFont(Font.font("Courier New", FontWeight.NORMAL, 15));
+		helpButton.setPrefHeight(40);
+		helpButton.setPrefWidth(150);
+		
+		home.getChildren().addAll(title, gameButton, helpButton);
 
-		this.getChildren().addAll(gameButton, helpButton);
+		this.getChildren().add(home);
+		
 	}
 
 	private void switchToGame() {
