2c4ce295ee719a68062118d8a11c23c901bb4983
Wisha Wa <wisha.wa@yandex.com>
2021-05-23 11:35:49 +0000

    docs for style button

--- src/gui/StyledButton.java
+++ src/gui/StyledButton.java
@@ -10,11 +10,26 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
+/**
+ * Button with our red-and-yellow style.
+ *
+ */
 public class StyledButton extends Button {
+	/**
+	 * Create a button with the specified text.
+	 * 
+	 * @param text The text content of the button.
+	 */
 	public StyledButton(String text) {
 		this(text, new CornerRadii(12));
 	}
 
+	/**
+	 * Create a button with the specified text and corner radii.
+	 * 
+	 * @param text  The text content of the button.
+	 * @param radii The corner radii.
+	 */
 	public StyledButton(String text, CornerRadii radii) {
 		super(text);
 		this.setBackground(new Background(new BackgroundFill(new Color(0.62, 0.22, 0.22, 1), radii, Insets.EMPTY)));
