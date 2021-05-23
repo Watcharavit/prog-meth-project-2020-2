d760544d6294e1597511156967afad2012ee86a1
Watcharavit Viwatsukpaisral <watcha.ea@hotmail.com>
2021-05-23 16:35:05 +0700

    delete doc files

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"entity"},{"l":"entity.bomb"},{"l":"entity.daemons"},{"l":"entity.drops"},{"l":"entity.equipments"},{"l":"entity.livings"},{"l":"entity.projectiles"},{"l":"entity.terrains"},{"l":"game"},{"l":"interfaces"},{"l":"logic"},{"l":"resources"}];updateSearchResults();
